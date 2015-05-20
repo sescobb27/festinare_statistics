2da105a6164d6026244c10375ea69191c99c4760
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2015-05-19 23:56:27 -0500

    Service Signals Setup

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/sescobb27/festinare_stadistics/database"
+)
+
+func signalHandler() {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan,
+		syscall.SIGINT,
+		syscall.SIGKILL,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+		syscall.SIGABRT,
+	)
+	<-signalChan
+	println("Closing DB Connection")
+	database.Session.Close()
+	os.Exit(0)
+}
