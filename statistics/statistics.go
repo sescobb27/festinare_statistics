49c94b86439f4733d65d9c508fe15935990e9808
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2015-05-19 23:56:47 -0500

    Main Functionality

---
+++ statistics/statistics.go
@@ -0,0 +1,34 @@
+package statistics
+
+import (
+	"time"
+
+	"github.com/sescobb27/festinare_stadistics/models"
+)
+
+// NumberOfRedeemedDiscounts Returns the number of redeemed discounts
+func NumberOfRedeemedDiscounts() {
+
+}
+
+// GroupByCategoryAndCount Returns how many times a category was redeemed
+func GroupByCategoryAndCount() {
+
+}
+
+// CompareCategories Returns in order (More Redeemed to Less Redeemed) the categories
+func CompareCategories(categories []*models.Category) []*models.Category {
+
+}
+
+func GetWeekDayForMostRedeems() {
+
+}
+
+func GetWeekDayForMostRedeemsForGivenCategory(category *models.Category) {
+
+}
+
+func GetMonthPeriodForMostRedeems(month time.Month) {
+
+}
